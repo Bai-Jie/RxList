f23e0931945b361d250193d59f78aa2caec70e7b
Bai Jie <baijie1991@gmail.com>
2016-03-21 12:47:42 +0800

    add ObservableList.java

---
+++ code/rxlist/src/main/java/gq/baijie/rxlist/ObservableList.java
@@ -0,0 +1,108 @@
+package gq.baijie.rxlist;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+import rx.subjects.Subject;
+
+@NotThreadSafe
+public class ObservableList<T> {
+
+  private final Subject<Event<T>, Event<T>> eventBus = PublishSubject.create();
+
+  private List<T> list;
+
+  public static <T> ObservableList<T> create(List<T> list) {
+    return new ObservableList<>(list);
+  }
+
+  public ObservableList(List<T> list) {
+    this.list = list;
+  }
+
+  public Observable<Event<T>> eventBus() {
+    return eventBus.onBackpressureDrop();
+  }
+
+  List<T> list() {
+    return Collections.unmodifiableList(list);
+  }
+
+  public void add(int index, T element) {
+    list.add(index, element);
+    eventBus.onNext(new AddEvent<>(this, index, element));
+  }
+
+  public void add(T o) {
+    add(list.size(), o);
+  }
+
+  public void remove(int index) {
+    T removed = list.remove(index);
+    eventBus.onNext(new RemoveEvent<>(this, index, removed));
+  }
+
+  public void remove(T o) {
+    int index = list.indexOf(o);
+    if (index >= 0) {
+      list.remove(index);
+    }
+  }
+
+  public void set(int index, T element) {
+    T oldElement = list.set(index, element);
+    eventBus.onNext(new SetEvent<>(this, index, oldElement, element));
+  }
+
+  public static class Event<T> {
+
+    public final ObservableList<T> observableList;
+
+    public Event(ObservableList<T> observableList) {
+      this.observableList = observableList;
+    }
+  }
+
+  public static class AddEvent<T> extends Event<T> {
+
+    public final int index;
+    public final T added;
+
+    public AddEvent(ObservableList<T> observableList, int index, T added) {
+      super(observableList);
+      this.index = index;
+      this.added = added;
+    }
+  }
+
+  public static class RemoveEvent<T> extends Event<T> {
+
+    public final int index;
+    public final T removed;
+
+    public RemoveEvent(ObservableList<T> observableList, int index, T removed) {
+      super(observableList);
+      this.index = index;
+      this.removed = removed;
+    }
+  }
+
+  public static class SetEvent<T> extends Event<T> {
+
+    public final int index;
+    public final T oldElement;
+    public final T newElement;
+
+    public SetEvent(ObservableList<T> observableList, int index, T oldElement, T newElement) {
+      super(observableList);
+      this.index = index;
+      this.oldElement = oldElement;
+      this.newElement = newElement;
+    }
+  }
+
+}
---
+++ code/rxlist/src/test/groovy/gq/baijie/rxlist/ObservableListSpecification.groovy
@@ -0,0 +1,4 @@
+package gq.baijie.rxlist
+
+class ObservableListSpecification {
+}
